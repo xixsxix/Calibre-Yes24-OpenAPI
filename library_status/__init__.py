0df46f1c66f8a3dd51bf098004a16cf24aa0b392
xixsxix <xixsxix.j@gmail.com>
2026-09-13 14:35:38 +0900

    Add public Library Status 0.3.5 plugin wrapper

---
+++ library_status/__init__.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""Calibre Interface Action wrapper for YES24 Library Status."""
+
+from calibre.customize import InterfaceActionBase
+
+
+class Yes24LibraryStatusPlugin(InterfaceActionBase):
+    name = "YES24 Library Status"
+    description = (
+        "Record YES24 bestseller, steady-seller, and ranking history in Calibre "
+        "custom columns. After Calibre starts, this plugin automatically connects "
+        "to the YES24 Open API to prefetch public ranking lists for fast automatic "
+        "status updates. It does not upload EPUB files or your library metadata."
+    )
+    supported_platforms = ["windows", "osx", "linux"]
+    author = "xixsxix"
+    version = (0, 3, 5)
+    minimum_calibre_version = (9, 0, 0)
+    actual_plugin = (
+        "calibre_plugins.yes24_library_status.ui:"
+        "Yes24LibraryStatusAction"
+    )
